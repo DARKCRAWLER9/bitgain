e4dc0a89a880022d9935765f5f23e9395bc9940f
DARKCRAWLER9 <148722391+DARKCRAWLER9@users.noreply.github.com>
2023-10-25 12:41:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+ <script type="text/javascript" src="web.js"></script>     
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/web3/1.2.7-rc.0/web3.min.js"></script>
+    <link rel="stylesheet" href="modal.css">    <link rel="stylesheet" href="modal.css">
+    <button id="connect" class="mintBtn" onclick="connectContract()">CONNECT</button>
